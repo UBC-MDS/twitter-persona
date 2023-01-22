e9a9c47f8736b1d163c0b8078001f1f80ec2fc51
Renzo Wijngaarden <renzorenzo55@gmail.com>
2023-01-22 10:49:32 -0800

    added wordcloud function and dependencies

--- src/twitterpersona/generate_word_cloud.py
+++ src/twitterpersona/generate_word_cloud.py
@@ -1,37 +1,66 @@
-def get_top_n_word(df, n):
-    """get top n words from a specific sentiment dataframe.
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS
+from twitterpersona.sentiment_analysis import count_tweets
+
+def create_wordcloud(df):
+    """
+    Create a wordcloud out of all the tweets of a user. 
+    Wordcloud colour is green if the overal sentiment of the
+    users tweets is positive, red if negative and blue if 
+    neutral.
+
     Parameters
     ----------
     df : pd.DataFrame
-        dataframe after sentiment_analysis
-    
-    n : int
-        how many number of top existing words in dataframe with tweets msg column
+        dataframe with all the tweets of a user, and their
+        sentiment analysis
     
     Returns
     -------
-    dict : dictionary
-        dictionary with key as word that has highest frequency and value as how many number it show up
+    plot : plt.figure()
+        a matplotlib plot of the wordcloud
+    
     Examples
     --------
-    get_top_n_word(df, 5)
+    create_wordcloud(df)
     """
 
-def create_wordcloud(text, output_path):
-    """Create Wordcloud.
-    Parameters
-    ----------
-    text : str
-        tweets message
+    # Import image to np.array
+    mask = np.array(Image.open('twitterLogo.png'))
     
-     output_path : str
-        the path that the newly generated png should located at
+    # Combine all tweets into single string
+    text = " ".join(tweet for tweet in df["text_clean"])
+
+    # Determine tweet sentiment
+    sentiment = count_tweets(df)
+
+    # Set wordcloud colour
+    if sentiment.idxmax() == 'positive':
+        colormap = 'summer'
+    elif sentiment.idxmax() == 'negative':
+        colormap = 'autumn'
+    else:
+        colormap = 'cool'
+
+    # Create the wordcloud
+    wordcloud = WordCloud(
+        width = 800, 
+        height = 800, 
+        min_font_size = 10, 
+        stopwords = STOPWORDS,
+        mask = mask,
+        background_color = 'white',
+        contour_color = '#00aced',
+        contour_width = 8,
+        colormap = colormap
+    ).generate(text)
+    fig = plt.figure(figsize = (8, 8), facecolor = None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad = 0)
+    plt.show()
     
-    Returns
-    -------
-    wordcount : png
-        the picture of wordcount
-    Examples
-    --------
-    create_wordcloud('today is bad day', /output)
-    """
+    return fig
