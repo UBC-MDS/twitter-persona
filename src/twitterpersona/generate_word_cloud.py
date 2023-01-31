458518e8ba084d64ce3d6e5a163cf31acb12ae34
Renzo Wijngaarden <renzorenzo55@gmail.com>
2023-01-31 11:32:07 -0800

    Updated functions and tests

--- src/twitterpersona/generate_word_cloud.py
+++ src/twitterpersona/generate_word_cloud.py
@@ -1,17 +1,66 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import requests
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS
 from twitterpersona.sentiment_analysis import count_tweets
 import urllib.request
 
 def transform_zeros(val):
+    """
+    Helper function to correct the mask array.
+
+    Parameters
+    ----------
+    val : int
+        Any integer.
+    
+    Returns
+    -------
+    val : int
+        255 if val is 0, and val otherwise.
+
+    Examples
+    --------
+    transform_zeros(0)
+    """
+
     if val == 0: 
        return 255
     else:
        return val
+
+def set_colourmap(df):
+    """
+    Determine the corresponding colourmap for a certain users
+    tweet sentiment analysis.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        A dataframe with all the tweets of a user, and their
+        sentiment analysis.
+    
+    Returns
+    -------
+    plot : plt.figure()
+        A matplotlib plot of the wordcloud.
+    
+    Examples
+    --------
+    set_colourmap(df)
+    """
+
+    # Determine tweet sentiment
+    sentiment = count_tweets(df)
+
+    # Set wordcloud colour
+    if max(sentiment, key=sentiment.get) == 'positive':
+        return('summer')
+    elif max(sentiment, key=sentiment.get) == 'negative':
+        return('autumn')
+    else:
+        return('cool')
 
 def create_wordcloud(df):
     """
@@ -48,19 +97,9 @@ def create_wordcloud(df):
     for i in range(len(mask)):
         maskable_image[i] = list(map(transform_zeros, mask[i]))
     mask = maskable_image
+
     # Combine all tweets into single string
     text = " ".join(tweet for tweet in df["text_clean"])
-
-    # Determine tweet sentiment
-    sentiment = count_tweets(df)
-
-    # Set wordcloud colour
-    if max(sentiment, key=sentiment.get) == 'positive':
-        colormap = 'summer'
-    elif max(sentiment, key=sentiment.get) == 'negative':
-        colormap = 'autumn'
-    else:
-        colormap = 'cool'
 
     # Create the wordcloud
     wordcloud = WordCloud(
@@ -72,7 +111,7 @@ def create_wordcloud(df):
         background_color = 'white',
         contour_color = '#00aced',
         contour_width = 8,
-        colormap = colormap
+        colormap = set_colourmap(df)
     ).generate(text)
     fig = plt.figure(figsize = (8, 8), facecolor = None)
     plt.imshow(wordcloud)
--- tests/twitterpersona/test_generate_word_cloud.py
+++ tests/twitterpersona/test_generate_word_cloud.py
@@ -1,35 +1,38 @@
-from twitterpersona.generate_word_cloud import create_wordcloud
+from twitterpersona.generate_word_cloud import create_wordcloud, set_colourmap, transform_zeros
 import pytest
 import pandas as pd
+import matplotlib
 
 @pytest.fixture
 def neg_input_df():
-    input = pd.DataFrame({'User': {0: 'Renzo', 1: 'Renzo', 2: 'Renzo', 3: 'Renzo'},
-                  'text': {0: 'I hate this place, I think everyone here is very rude!',
-                            1: "I've never enjoyed something as simple and mundane as this game.",
-                            2: "Very often I just can't see the fun in something when other people do, it's like I just don't know how to have fun.",
-                            3: "I don't think this administration has accomplished anything noteworthy in the two years they've been in power. It's a disgrace!",
-                            }})
+    input = pos_df = pd.DataFrame({"User":{"0":"Renzo","1":"Renzo","2":"Renzo","3":"Renzo"},"text":{"0":"I hate this place, I think everyone here is very rude!","1":"I\'ve never enjoyed something as simple and mundane as this game.","2":"Very often I just can\'t see the fun in something when other people do, it\'s like I just don\'t know how to have fun.","3":"I don\'t think this administration has accomplished anything noteworthy in the two years they\'ve been in power. It\'s a disgrace!"},"text_clean":{"0":"I hate place, I think everyone rude!","1":"I\'ve never enjoyed something simple mundane game.","2":"Very often I can\'t see fun something people do, like I know fun.","3":"I think administration accomplished anything noteworthy two years they\'ve power. It\'s disgrace!"},"sentiment":{"0":"negative","1":"negative","2":"negative","3":"negative"}})
     return input
 
 @pytest.fixture
 def pos_input_df():
-    input = pd.DataFrame({'User': {0: 'Timmy', 1: 'Timmy', 2: 'Timmy', 3: 'Timmy'},
-                  'text': {0: 'I absolutely love it here! I think moving here was one of the best decisions of my life.',
-                            1: "This is an incredibly exciting game and I recommend that everyone who likes this genre gives it a go!",
-                            2: "I went out with my firends last night: we het the town, had some drinks, and ended up doing karaoke! Such a good time",
-                            3: "I received a really good grade for my python package! I'm so happy!"
-                            }})
+    input = pos_df = pd.DataFrame({"User":{"0":"Timmy","1":"Timmy","2":"Timmy","3":"Timmy"},"text":{"0":"I absolutely love it here! I think moving here was one of the best decisions of my life.","1":"This is an incredibly exciting game and I recommend that everyone who likes this genre gives it a go!","2":"I went out with my firends last night: we het the town, had some drinks, and ended up doing karaoke! Such a good time","3":"I received a really good grade for my python package! I\'m so happy!"},"text_clean":{"0":"I absolutely love here! I think moving one best decisions life.","1":"This incredibly exciting game I recommend everyone likes genre gives go!","2":"I went firends last night: het town, drinks, ended karaoke! Such good time","3":"I received really good grade python package! I\'m happy!"},"sentiment":{"0":"positive","1":"positive","2":"positive","3":"positive"}})
     return input
 
-def text_colour(neg_input_df):
+def test_fig_size(neg_input_df):
     wordcloud = create_wordcloud(neg_input_df)
-    assert wordcloud.get_figheight == 800
+    assert wordcloud.get_figheight() == 8.0
 
-def text_colour(neg_input_df):
+def test_fig_size(neg_input_df):
     wordcloud = create_wordcloud(neg_input_df)
-    assert wordcloud.colormap == 'autumn'
+    assert type(wordcloud) is matplotlib.figure.Figure
 
-def text_colour(pos_input_df):
-    wordcloud = create_wordcloud(pos_input_df)
-    assert wordcloud.colormap == 'summer'
+def test_neg_colour(neg_input_df):
+    colourmap = set_colourmap(neg_input_df)
+    assert colourmap == 'autumn'
+
+def test_pos_colour(pos_input_df):
+    colourmap = set_colourmap(pos_input_df)
+    assert colourmap == 'summer'
+
+def test_transform_zeros_zero():
+    val = transform_zeros(0)
+    assert val == 255
+
+def test_transform_zeros_val():
+    val = transform_zeros(524)
+    assert val == 524
